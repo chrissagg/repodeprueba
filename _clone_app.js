5d9188ac69acd282fd75892d7177f073e99e6a71
chrissagg <chriss.agg@outlook.com>
2022-05-28 12:18:47 -0300

    commit-inicial

--- _clone_app.js
+++ _clone_app.js
@@ -1,1 +1,9 @@
+const user = {
+  email: "arteenmelamina@gmail.com",
+  pass: "BLS154__jkv",
+  alias: "SierraLoka",
+}
 
+let saludoLogin = `Hola ${user.alias} cómo estás hoy?`
+
+console.log(saludoLogin)
