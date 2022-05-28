aa2e775b63bcabace3fc838158ad25d2e9036205
chrissagg <chriss.agg@outlook.com>
2022-05-28 12:21:41 -0300

    agregado email de recuperacion

--- _clone_app.js
+++ _clone_app.js
@@ -2,6 +2,7 @@ const user = {
   email: "arteenmelamina@gmail.com",
   pass: "BLS154__jkv",
   alias: "SierraLoka",
+  emailRec: "serverstatus@gmail.com",
 }
 
 let saludoLogin = `Hola ${user.alias} cómo estás hoy?`
